0951ca2c1731e7847e3164ec6266f1c89bec187a
Juan Sebastian Peña <jspenaq@unal.edu.co>
2019-05-15 21:23:44 -0500

    add updatePassword(), now an user can change his password

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/api");
 const redisConnect = require("./redisConnect");
-	
+const postgres = require("./postgres");
+
 const port = process.env.PORT || 3000;
 const app = express();
 
 // parse application/json
 app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
 
 app.listen(port, function() {
 	console.log(`Hey, listen in ${port}`);
--- postgres.js
+++ postgres.js
@@ -3,44 +3,46 @@ const { Pool, Client } = require("pg");
 // pools will use environment variables
 // for connection information
 const pool = new Pool({
-	user: "shibaby",
-	host: "localhost",
-	database: "shibaby_files",
-	password: "12345",
-	port: 5432
-});
-
-// pool.query("SELECT NOW()", (err, res) => {
-// 	console.log(err, res);
-// 	pool.end();
-// });
-
-// clients will also use environment variables
-// for connection information
-const client = new Client({
 	user: "postgres",
 	host: "localhost",
-    database: "linuxhint",
-    // password: "12345"
+	database: "users-db",
+	password: "123456",
 	port: 5432
 });
-client.connect();
 
-// let query = 'CREATE TABLE users (    firstname   VARCHAR(20),lastname    VARCHAR(20))';
-
-const query = {
-	text: "SELECT * FROM USERS"
-};
-
-
-client.query(query, (err, res) => {
-	if (err) {
-		console.log("--- err ---");
-		console.log(err);
-	} else {
-		console.log(res.rows[0]);
+function printAllUsers() {
+	let query = {
+		text: "SELECT * FROM users"
 	}
-	client.end();
-});
 
-// --build-arg HTTP_PROXY=http://168.176.239.41:8080 --build-arg HTTPS_PROXY=http://168.176.239.41:8080
+	pool.query(query, (err, res) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(res.rows);
+		}
+		pool.end();
+	});
+}
+
+function updatePassword(userName, password) {
+	let query = {
+		text: `UPDATE users
+		SET password = $2
+		WHERE user_name = $1`,
+		values: [userName, password]
+	};
+	pool.query(query, (err, res) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("Postgres: Contraseña cambiada");
+		}
+		pool.end();
+	});
+}
+
+// // --build-arg HTTP_PROXY=http://168.176.239.41:8080 --build-arg HTTPS_PROXY=http://168.176.239.41:8080
+
+// updatePassword("user4","secret4")
+printAllUsers()
