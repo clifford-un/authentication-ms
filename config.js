80a585315f62fadf6d6c0a2ebcd5113dc72717da
Juan Sebastian Peña <36420972+jspenaq@users.noreply.github.com>
2019-06-18 22:51:19 -0500

    :clock3: more time for "Expiration_Time"

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
     // To make the JWT more efficient we need 3 things:
     Issuer: "Clifford-UN", // Issuer (Software organization who issues the token)
     Audience: "Clifford-UN", // Audience (Domain within which this token will live and function)
-    Expiration_Time: "20m",
+    Expiration_Time: "1d",
     Algorithm: "HS256"
 }
